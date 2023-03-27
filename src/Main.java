1928984b46192868c71cdf0365ef6ea8589f5a95
tomasdias10 <tomasdias333@gmail.com>
2023-03-27 19:59:22 +0100

    traduzido para PT e atualizado!!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("#vski!");
+        System.out.println("Ola mundo!");
+        System.out.println("Boas");
     }
 }
